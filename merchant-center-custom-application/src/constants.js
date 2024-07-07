729459c14ac08146b53f434b8b1a12b703b83486
jspteampaydock <157017480+jspteampaydock@users.noreply.github.com>
2024-07-07 16:29:47 +0300

    Update constants.js

--- merchant-center-custom-application/src/constants.js
+++ merchant-center-custom-application/src/constants.js
@@ -1,7 +1,7 @@
 // Make sure to import the helper functions from the `ssr` entry point.
 import { entryPointUriPathToPermissionKeys } from '@commercetools-frontend/application-shell/ssr';
 
-export const entryPointUriPath = 'paydock_app';
+export const entryPointUriPath = 'dev-paydock-app';
 
 export const PERMISSIONS = entryPointUriPathToPermissionKeys(entryPointUriPath);
 
